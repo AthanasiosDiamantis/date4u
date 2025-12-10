1d96610ae466e6b031f3462a28b7cf9f8221b3d2
Athanasios Diamantis MBP <saki.diamantis@web.de>
2025-12-10 17:24:15 +0100

    add progress to spring project, just show in console what is automatically done by spring boot3

--- src/main/java/com/saki/date4u/Date4uApplication.java
+++ src/main/java/com/saki/date4u/Date4uApplication.java
@@ -2,12 +2,18 @@ package com.saki.date4u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Date4uApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Date4uApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Date4uApplication.class, args);
+        Arrays.stream(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
+
+
     }
 
 }
