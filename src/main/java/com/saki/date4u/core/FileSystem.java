53428bf6450f6b8d2a115c6e0fdd56bc1b40f820
Athanasios Diamantis MBP <saki.diamantis@web.de>
2025-12-11 16:01:43 +0100

    add class to organize file system storage

---
+++ src/main/java/com/saki/date4u/core/FileSystem.java
@@ -0,0 +1,48 @@
+package com.saki.date4u.core;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author athanasios.diamantis
+ * @since 1.0.0
+ * this is the 1st class I introduced to organize the photos in the file system
+ */
+public class FileSystem {
+
+    private final Path root = Paths.get(System.getProperty("user.home"), "fs");
+
+    public FileSystem() {
+        try {
+            if (!Files.isDirectory(root)) {
+                Files.createDirectory(root);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public long getFreeDiskSpace() {
+        return root.toFile().getFreeSpace();
+    }
+
+    public byte[] load(String filename) {
+        try {
+            return Files.readAllBytes(root.resolve(filename));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public void store(String filename, byte[] bytes) {
+        try {
+            Files.write(root.resolve(filename), bytes);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+}
