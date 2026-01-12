9a48f3c359d4b52a5236f306bd948506b2630fc3
Athanasios Diamantis MBP <saki.diamantis@web.de>
2026-01-12 16:56:50 +0100

    add disk space and experimental toLowercase method to shell class

--- src/main/java/com/saki/date4u/interfaces/shell/FsCommands.java
+++ src/main/java/com/saki/date4u/interfaces/shell/FsCommands.java
@@ -1,13 +1,27 @@
 package com.saki.date4u.interfaces.shell;
 
+import com.saki.date4u.core.FileSystem;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.util.unit.DataSize;
 
 @ShellComponent /*this annotation is responsable that this shell command can be used*/
 public class FsCommands {
 
+    private final FileSystem fs = new FileSystem();
+
+    @ShellMethod("Display free disk space")
+    public String freeDiskSpace() {
+        return DataSize.ofBytes( fs.getFreeDiskSpace())
+                .toGigabytes() + "GB";
+    }
     @ShellMethod( "Display required free disk space")
     public long minimumFreeDiskSpace() {
         return 1_000_000;
+    }
+
+    @ShellMethod("Convert to lowercase string")
+    public String toLowercase(String input) {
+        return input.toLowerCase();
     }
 }
